7eeb14c29162db2efbd83742e25451afa82bfcf8
ALEXANDRE LIMA <alexandre.lima0451@gmail.com>
2022-03-17 20:08:12 -0300

    commit inicial

---
+++ src/test/java/br/com/rest/internetbanking/InternetBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.internetbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
